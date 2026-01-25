03be520c17bdf33f4310a006e5f537ebe5a30773
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-01-25 08:10:57 +0530

    first commit

--- Express/Class1/index.js
+++ Express/Class1/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const { type } = require('os');
+const morgan = require('morgan');    // (third party middleware) HTTP request logger middleware for node.js 
 
 const index = fs.readFileSync('index.html', 'utf-8');
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
@@ -18,12 +18,15 @@ const server = express();  // to create server
 // Middleware 
 
 server.use(express.json());   // body Parser
+server.use(morgan('dev'));
 server.use(express.static('public')); // serves static assets such as HTML files, images, and so on.
 
-server.use((req, res, next) => {
-    console.log(req.method, req.ip, req.hostname);
-    next();  // move to next step
-});
+
+
+// server.use((req, res, next) => {
+//     console.log(req.method, req.ip, req.hostname);
+//     next();  // move to next step
+// });
 
 
 
@@ -65,6 +68,12 @@ server.delete('/', auth, (req, res) => {
     res.json({ type: 'Delete'});
 });
 
+server.get('/demo', (req, res) => {
+  // res.sendStatus(404);
+  // res.json(products)
+  // res.status(201).send('<h1>hello</h1>')
+  // res.sendFile('/Users/abhishekrathore/Desktop/node-app/index.html')
+});
 
 server.listen(8080, () => {
     console.log("Server Started")
