a353f694a8658aedceea3f1ac58823850ec50077
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-01-26 12:29:21 +0530

    MVC (Model View Controller)

--- MVC/Class1/Index.js
+++ MVC/Class1/Index.js
@@ -1,27 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
-const productController = require("./controller/product");
-
-// Adding router
-const productRouter = express.Router();
+const productRouter = require('./routes/product')
+const userRouter = require('./routes/user')
 
 //bodyParser
 server.use(express.json());
-server.use(morgan('default'))
+server.use(morgan('default'));
 server.use(express.static('public'));
-server.use('/', productRouter);
-
-// MVC(Model View  Controller)
-//Create POST / products   
-server
-    .post("/products", productController.createProduct)
-    .get("/products", productController.getProduct)
-    .get("/products/:id", productController.getProductById)
-    .put("/products/:id", productController.updateAllFeatureProduct)
-    .patch("/products/:id", productController.updateSomeFeatureProduct)
-    .delete("/products/:id", productController.deleteProduct);
-
+server.use('/products',productRouter.router);
+server.use('/users',userRouter.router);
 
 server.listen(8080, () => {
   console.log('server started');
