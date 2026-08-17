647496bc28c19aefaac3935c1354b45f9e1bc853
Satyam6201 <satyamkmishraa@gmail.com>
2026-08-17 12:58:42 +0530

    Adding LLM

--- AI-Backend/level1/index.js
+++ AI-Backend/level1/index.js
@@ -5,6 +5,7 @@ import { GoogleGenAI } from "@google/genai";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
@@ -12,11 +13,23 @@ const ai = new GoogleGenAI({
     apiKey: process.env.GEMINI_API_KEY,
 });
 
-const interaction = await ai.interactions.create({
-  model: "gemini-3.6-flash",
-  input: "Explain how AI works in a few words",
-});
-console.log(interaction.output_text);
+app.post("/ai", async (req, res) => {
+    const { input } = req.body;
+    const response = await ai.models.generateContent({
+        model: "gemini-3.6-flash",
+        contents: [
+            {
+                role: "user",
+                parts: [{text: input}]
+            },
+        ],
+        config: {
+                systemInstruction: "You are my AI Agent and your name is MultiAgent."
+            }
+    });
+
+    return res.status(200).json(response.text);
+})
 
 app.get("/", async (req, res) => {
     return res.status(200).json({message: "Server is started"});
